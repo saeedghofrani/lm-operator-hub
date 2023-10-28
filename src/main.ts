db3667bedb5d65871b06c33abedf7ec91bd9eef0
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-29 02:57:51 +0330

    main => add logger to main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './api/app/app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000).then(async () => {
+    Logger.log(
+      `http://127.0.0.1:3000`,
+      'Running Server',
+    );
+  });
 }
 bootstrap();
