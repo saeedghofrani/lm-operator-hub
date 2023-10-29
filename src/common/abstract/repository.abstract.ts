fc6cf71caa29d0cd10bfc1cf6b93809acbfebbd4
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-29 13:16:54 +0330

    repository => fix methods promise

--- src/common/abstract/repository.abstract.ts
+++ src/common/abstract/repository.abstract.ts
@@ -4,23 +4,23 @@ export abstract class BaseRepository<T, C, A> {
     constructor(protected prisma: PrismaService) { }
 
     async create(data: C): Promise<T> {
-        return this.prisma[this.modelName].create({ data });
+        return await this.prisma[this.modelName].create({ data });
     }
 
     async findAll(): Promise<T[]> {
-        return this.prisma[this.modelName].findMany({});
+        return await this.prisma[this.modelName].findMany({});
     }
 
     async findOne(id: number): Promise<T | null> {
-        return this.prisma[this.modelName].findUnique({ where: { id } });
+        return await this.prisma[this.modelName].findUnique({ where: { id } });
     }
 
     async update(id: number, data: Partial<A>): Promise<T | null> {
-        return this.prisma[this.modelName].update({ where: { id }, data });
+        return await this.prisma[this.modelName].update({ where: { id }, data });
     }
 
     async remove(id: number): Promise<T | null> {
-        return this.prisma[this.modelName].delete({ where: { id } });
+        return await this.prisma[this.modelName].delete({ where: { id } });
     }
 
     abstract get modelName(): string;
