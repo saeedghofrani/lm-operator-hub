b163b122c5e599d0f54419ef50f647bc9a5ac4ff
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-29 18:54:18 +0330

    app => import user module

--- src/api/app/app.module.ts
+++ src/api/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './controller/app.controller';
 import { AppService } from './service/app.service';
 import { RoleModule } from '../role/role.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [RoleModule],
+  imports: [RoleModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
