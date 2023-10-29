d6ca4b894a857a7fa6213cb7a13781d767681953
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-29 17:33:55 +0330

    repository => implement deleted conditions

--- src/common/abstract/repository.abstract.ts
+++ src/common/abstract/repository.abstract.ts
@@ -1,18 +1,20 @@
 import { PrismaService } from 'src/prisma/prisma.service';
 
 export abstract class BaseRepository<T, C, A> {
-    constructor(protected prisma: PrismaService) { }
+    constructor(
+        protected prisma: PrismaService
+    ) { }
 
     async create(data: C): Promise<T> {
         return await this.prisma[this.modelName].create({ data });
     }
 
     async findAll(): Promise<T[]> {
-        return await this.prisma[this.modelName].findMany({});
+        return await this.prisma[this.modelName].findMany({ where: { deleted: null } });
     }
 
     async findOne(id: number): Promise<T | null> {
-        return await this.prisma[this.modelName].findUnique({ where: { id } });
+        return await this.prisma[this.modelName].findUnique({ where: { id, deleted: null } });
     }
 
     async update(id: number, data: Partial<A>): Promise<T | null> {
@@ -20,7 +22,15 @@ export abstract class BaseRepository<T, C, A> {
     }
 
     async remove(id: number): Promise<T | null> {
-        return await this.prisma[this.modelName].delete({ where: { id } });
+        const existingRecord = await this.prisma[this.modelName].findUnique({
+            where: { id },
+        });
+        if (!existingRecord)
+            return null;
+        return this.prisma[this.modelName].update({
+            where: { id },
+            data: { deleted: new Date() },
+        });
     }
 
     abstract get modelName(): string;
