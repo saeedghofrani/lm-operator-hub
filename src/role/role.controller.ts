344f44cfff63ea7a0c6e4107ca3dc3dc1da08928
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-29 06:20:05 +0330

    role => swagger ok response

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -2,34 +2,40 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { RoleService } from './role.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { RoleEntity } from './entities/role.entity';
 
 @Controller('role')
-@ApiTags('articles')
+@ApiTags('roles')
 export class RoleController {
-  constructor(private readonly roleService: RoleService) {}
+  constructor(private readonly roleService: RoleService) { }
 
   @Post()
+  @ApiCreatedResponse({ type: RoleEntity })
   create(@Body() createRoleDto: CreateRoleDto) {
     return this.roleService.create(createRoleDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: RoleEntity, isArray: true })
   findAll() {
     return this.roleService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: RoleEntity })
   findOne(@Param('id') id: string) {
     return this.roleService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({ type: RoleEntity })
   update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
     return this.roleService.update(+id, updateRoleDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: RoleEntity })
   remove(@Param('id') id: string) {
     return this.roleService.remove(+id);
   }
