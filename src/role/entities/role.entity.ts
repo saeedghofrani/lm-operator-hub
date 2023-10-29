78e97240a6f4705173a6fe0c4dc0ff06c433fbd7
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-29 06:16:56 +0330

    role => define role entity

--- src/role/entities/role.entity.ts
+++ src/role/entities/role.entity.ts
@@ -1,1 +1,8 @@
-export class Role {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Role } from 'prisma/prisma-client'
+
+export class RoleEntity implements Role {
+    @ApiProperty() id: number;
+    @ApiProperty() name: string;
+    @ApiProperty({ required: false, nullable: true }) default: boolean;
+}
