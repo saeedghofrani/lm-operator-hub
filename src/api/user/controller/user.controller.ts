1b6b1e70c6717696de5375f847a0f562fe62c747
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-29 18:54:01 +0330

    user => controller complete

---
+++ src/api/user/controller/user.controller.ts
@@ -0,0 +1,48 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { PaginationQueryDto } from 'src/common/pagination/dto/query.dto';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { UpdateUserDto } from '../dto/update-user.dto';
+import { UserEntity } from '../entities/user.entity';
+import { UserService } from '../service/user.service';
+
+@Controller('user')
+@ApiTags('users')
+export class UserController {
+  constructor(private readonly userService: UserService) { }
+
+  @Post()
+  @ApiCreatedResponse({ type: UserEntity })
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  @ApiOkResponse({ type: UserEntity, isArray: true })
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get('page')
+  pagination(@Query() paginationQueryDto: PaginationQueryDto) {
+    return this.userService.pagination(paginationQueryDto);
+  }
+
+  @Get(':id')
+  @ApiOkResponse({ type: UserEntity })
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(+id);
+  }
+
+  @Patch(':id')
+  @ApiOkResponse({ type: UserEntity })
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  @ApiOkResponse({ type: UserEntity })
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
+  }
+}
---
+++ src/api/user/user.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { UserService } from './service/user.service';
+import { UserController } from './controller/user.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { UserRepository } from './repository/user.repository';
+import { PaginationService } from 'src/common/pagination/service/create.service';
+import { PasswordHasherService } from 'src/utilities/crypto/service/hash.service';
+import { RoleModule } from '../role/role.module';
+
+@Module({
+  imports: [PrismaModule, RoleModule],
+  controllers: [UserController],
+  providers: [UserService, UserRepository, PaginationService, PasswordHasherService],
+})
+export class UserModule {}
