b58f23aeb6fabc13490c874a1f4bafb90dc5d349
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-29 17:34:18 +0330

    role => provide pagination service

--- src/api/role/role.module.ts
+++ src/api/role/role.module.ts
@@ -3,10 +3,11 @@ import { RoleService } from './service/role.service';
 import { RoleController } from './controller/role.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { RoleRepository } from './repository/role.repository';
+import { PaginationService } from 'src/common/pagination/service/create.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [RoleController],
-  providers: [RoleService, RoleRepository],
+  providers: [RoleService, RoleRepository, PaginationService],
 })
 export class RoleModule {}
