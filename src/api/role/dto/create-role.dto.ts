fe9c350ff3682268b63a4485ee562f6e18cf3220
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-29 07:41:03 +0330

    role => dto validation

--- src/api/role/dto/create-role.dto.ts
+++ src/api/role/dto/create-role.dto.ts
@@ -1,11 +1,14 @@
-// src/articles/dto/create-article.dto.ts
-
 import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
+import { RoleEntity } from '../entities/role.entity';
 
-export class CreateRoleDto {
+export class CreateRoleDto implements Partial<RoleEntity> {
     @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
     name: string;
 
     @ApiProperty({ required: false, default: false })
+    @IsBoolean()
     default?: boolean = false;
 }
