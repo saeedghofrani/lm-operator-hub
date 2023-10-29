1794e0699fd9076aeb37f1ddbc756a1407bbd11d
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-29 17:34:32 +0330

    role => add deleted column

--- src/api/role/entities/role.entity.ts
+++ src/api/role/entities/role.entity.ts
@@ -4,6 +4,7 @@ import { BaseEntity } from "src/common/entities/base.entity";
 
 export class RoleEntity extends BaseEntity implements Role {
     @ApiProperty() id: number;
+    @ApiProperty({ required: false, default: false }) deleted: Date;
     @ApiProperty() name: string;
     @ApiProperty({ required: false, default: false }) default: boolean;
 }
