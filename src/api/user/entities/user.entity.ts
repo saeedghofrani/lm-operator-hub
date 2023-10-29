b583eeb20d3df513192e69f130afbb878207f480
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-29 18:51:49 +0330

    user => define user entity

---
+++ src/api/user/entities/user.entity.ts
@@ -0,0 +1,11 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Role, User } from 'prisma/prisma-client'
+import { BaseEntity } from "src/common/entities/base.entity";
+
+export class UserEntity extends BaseEntity implements User {
+    @ApiProperty() id: number;
+    @ApiProperty({ required: false, default: false }) deleted: Date;
+    @ApiProperty() email: string;
+    @ApiProperty() username: string;
+    @ApiProperty() password: string;
+}
