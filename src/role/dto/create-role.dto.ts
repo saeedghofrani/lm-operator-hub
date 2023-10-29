4f19ed90b149c41e4787efc1d18dcb1f7578ff80
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-29 06:09:29 +0330

    role => define role dtos

--- src/role/dto/create-role.dto.ts
+++ src/role/dto/create-role.dto.ts
@@ -1,1 +1,11 @@
-export class CreateRoleDto {}
+// src/articles/dto/create-article.dto.ts
+
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateRoleDto {
+    @ApiProperty()
+    name: string;
+
+    @ApiProperty({ required: false, default: false })
+    default?: boolean = false;
+}
--- src/role/dto/update-role.dto.ts
+++ src/role/dto/update-role.dto.ts
@@ -1,4 +1,10 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateRoleDto } from './create-role.dto';
 
-export class UpdateRoleDto extends PartialType(CreateRoleDto) {}
+export class UpdateRoleDto extends PartialType(CreateRoleDto) {
+    @ApiProperty()
+    name: string;
+
+    @ApiProperty({ required: false, default: false })
+    default?: boolean = false;
+}
