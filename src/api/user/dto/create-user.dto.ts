8a5f97199defa5c5edc2229d677be6d52c7c515c
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-29 18:51:23 +0330

    user => define dtos

---
+++ src/api/user/dto/create-user.dto.ts
@@ -0,0 +1,31 @@
+import { ApiHideProperty, ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { UserEntity } from '../entities/user.entity';
+
+export class CreateUserDto implements Partial<UserEntity> {
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    email: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    username: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    password: string;
+
+    @ApiHideProperty()
+    roles: {
+        create: Array<{
+            role: {
+                connect: {
+                    id: number;
+                };
+            };
+        }>;
+    }
+}
