dead5ba4481b90be51c4ee53292ccaaadc731dc4
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-29 20:56:28 +0330

    product => define product module

---
+++ src/api/product/product.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { PaginationService } from 'src/common/pagination/service/create.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { ProductController } from './controller/product.controller';
+import { ProductRepository } from './repository/product.repository';
+import { ProductService } from './service/product.service';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [ProductController],
+  providers: [ProductService, ProductRepository, PaginationService],
+  exports: [ProductService]
+})
+export class ProductModule {}
