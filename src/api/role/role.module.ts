bead4be111c79f4f352142a41627c146074d6faa
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-29 07:40:30 +0330

    role => add role repository

--- src/api/role/role.module.ts
+++ src/api/role/role.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { RoleService } from './service/role.service';
 import { RoleController } from './controller/role.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { RoleRepository } from './repository/role.repository';
 
 @Module({
   imports: [PrismaModule],
   controllers: [RoleController],
-  providers: [RoleService],
+  providers: [RoleService, RoleRepository],
 })
 export class RoleModule {}
