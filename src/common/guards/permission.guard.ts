d767751bade24cdd534dfe383af7c90f97a981dc
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-31 01:52:37 +0330

    guard => write comments

--- src/common/guards/permission.guard.ts
+++ src/common/guards/permission.guard.ts
@@ -1,6 +1,5 @@
-import { CanActivate, ExecutionContext, ForbiddenException, Inject, UnauthorizedException, UseGuards } from "@nestjs/common";
+import { CanActivate, ExecutionContext, ForbiddenException, Inject, UnauthorizedException } from "@nestjs/common";
 import { PermissionService } from "src/api/permission/service/permission.service";
-import { UserInterface } from "../interfaces/user.interface";
 import { Reflector } from "@nestjs/core";
 import { IS_PUBLIC_PERMISSION_KEY } from "../constant/public-permission.constant";
 
@@ -11,6 +10,7 @@ export class PermissionGuard implements CanActivate {
     ) { }
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
+        // Check if the route is marked as public.
         const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_PERMISSION_KEY, [
             context.getHandler(),
             context.getClass(),
@@ -18,28 +18,43 @@ export class PermissionGuard implements CanActivate {
         if (isPublic) {
             return true;
         }
+
         const request = context.switchToHttp().getRequest();
+        const permissions: number[] = request['user']['permissions'] || [];
+
+        // Ensure the user has permissions.
+        if (permissions.length < 1) {
+            throw new UnauthorizedException('You are not authorized');
+        }
+
         let address: string = String(request.originalUrl).split('?')[0];
         const method = request.method;
-        const permissions: number[] = request['user']['permissions'] || [];
-        if (permissions.length < 1)
-            throw new UnauthorizedException('you are not authorized');
+
+        // Replace dynamic segments in the address with {param}.
         address = address.replace(/\/\d+/g, (match) => {
             const param = this.getPropertiesByValue(request.params, match.substring(1));
-            return `/{${param[0]}}`
+            return `/{${param[0]}}`;
         });
+
         let permission = await this.permissionService.findByAddress(address, method);
+
+        // Restore the original address.
         address = address.replace(/\/\d+/g, (match) => {
             const param = this.getPropertiesByValue(request.params, match.substring(1));
-            return `/${param[0]}`
+            return `/${param[0]}`;
         });
-        if (!permission)
-            throw new UnauthorizedException('you are not authorized');
-        const access = permissions.find(item => item == permission.id);
-        if (!access)
+
+        if (!permission) {
+            throw new UnauthorizedException('You are not authorized');
+        }
+
+        const access = permissions.includes(permission.id);
+
+        if (!access) {
             throw new ForbiddenException('Forbidden resource');
-        else
-            return true;
+        }
+
+        return true;
     }
 
     getPropertiesByValue(obj, valueToFind) {
