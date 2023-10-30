225b5fb00f3181c7e86d9dd7c6068cfdead78e8a
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-30 09:34:07 +0330

    order => define dtos

---
+++ src/api/order/dto/create-order.dto.ts
@@ -0,0 +1,22 @@
+import { ApiHideProperty, ApiProperty } from '@nestjs/swagger';
+import { OrderEntity } from '../entities/order.entity';
+import { IsNotEmpty, IsNumber } from 'class-validator';
+
+export class CreateOrderDto implements Partial<OrderEntity> {
+    @ApiHideProperty() orderNumber: string;
+    @ApiHideProperty() orderTime: Date;
+    @ApiHideProperty() dueTime: Date;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsNumber()
+    productId: number;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsNumber()
+    userId: number;
+
+    @ApiHideProperty() product: Record<string, any>;
+    @ApiHideProperty() assignedTo: Record<string, any>;
+}
---
+++ src/api/order/dto/update-order.dto.ts
@@ -0,0 +1,19 @@
+import { ApiHideProperty, ApiProperty, PartialType } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber } from 'class-validator';
+import { CreateOrderDto } from './create-order.dto';
+
+export class UpdateOrderDto extends PartialType(CreateOrderDto) {
+    @ApiHideProperty() orderNumber: string;
+    @ApiHideProperty() orderTime: Date;
+    @ApiHideProperty() dueTime: Date;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsNumber()
+    productId: number;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsNumber()
+    userId: number;
+}
