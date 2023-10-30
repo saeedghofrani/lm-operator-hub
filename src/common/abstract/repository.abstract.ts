24198a9c6292fcc25b4bca8a134c496223264f83
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-30 09:33:26 +0330

    repository  => wrap methods in try catch

--- src/common/abstract/repository.abstract.ts
+++ src/common/abstract/repository.abstract.ts
@@ -6,31 +6,52 @@ export abstract class BaseRepository<T, C, A> {
     ) { }
 
     async create(data: C): Promise<T> {
-        return await this.prisma[this.modelName].create({ data });
+        try {
+            return await this.prisma[this.modelName].create({ data });
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
     }
 
     async findAll(): Promise<T[]> {
-        return await this.prisma[this.modelName].findMany({ where: { deleted: null } });
+        try {
+            return await this.prisma[this.modelName].findMany({ where: { deleted: null } });
+        } catch (error) {
+            throw error;
+        }
     }
 
     async findOne(id: number): Promise<T | null> {
-        return await this.prisma[this.modelName].findUnique({ where: { id, deleted: null } });
+        try {
+            return await this.prisma[this.modelName].findUnique({ where: { id, deleted: null } });
+        } catch (error) {
+            throw error;
+        }
     }
 
     async update(id: number, data: Partial<A>): Promise<T | null> {
-        return await this.prisma[this.modelName].update({ where: { id }, data });
+        try {
+            return await this.prisma[this.modelName].update({ where: { id }, data });
+        } catch (error) {
+            throw error;
+        }
     }
 
     async remove(id: number): Promise<T | null> {
-        const existingRecord = await this.prisma[this.modelName].findUnique({
-            where: { id },
-        });
-        if (!existingRecord)
-            return null;
-        return this.prisma[this.modelName].update({
-            where: { id },
-            data: { deleted: new Date() },
-        });
+        try {
+            const existingRecord = await this.prisma[this.modelName].findUnique({
+                where: { id },
+            });
+            if (!existingRecord)
+                return null;
+            return this.prisma[this.modelName].update({
+                where: { id },
+                data: { deleted: new Date() },
+            });
+        } catch (error) {
+            throw error;
+        }
     }
 
     abstract get modelName(): string;
