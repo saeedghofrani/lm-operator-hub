64f2baf32ab95e0ecbc8a2d9f2d0d6dded4831de
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-31 01:10:59 +0330

    hash => add bcrypt salt round to env

--- src/utilities/crypto/service/hash.service.ts
+++ src/utilities/crypto/service/hash.service.ts
@@ -1,18 +1,33 @@
 // password-hasher.service.ts
+
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class PasswordHasherService {
-  async hashPassword(password: string): Promise<string> {
-    const saltRounds = 10;
-    return bcrypt.hash(password, saltRounds);
+  private readonly saltRounds: number;
+
+  constructor(configService: ConfigService) {
+    this.saltRounds = configService.get('bcrypt_salt_rounds') || 10; // Replace 'bcrypt_salt_rounds' with your configuration key
   }
 
-  async comparePasswords(
-    plainTextPassword: string,
-    hashedPassword: string,
-  ): Promise<boolean> {
+  /**
+   * Hashes a password using bcrypt.
+   * @param password - The password to be hashed.
+   * @returns A promise that resolves to the hashed password.
+   */
+  async hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, this.saltRounds);
+  }
+
+  /**
+   * Compares a plain text password with a hashed password.
+   * @param plainTextPassword - The plain text password to compare.
+   * @param hashedPassword - The hashed password to compare against.
+   * @returns A promise that resolves to a boolean indicating whether the passwords match.
+   */
+  async comparePasswords(plainTextPassword: string, hashedPassword: string): Promise<boolean> {
     return bcrypt.compare(plainTextPassword, hashedPassword);
   }
 }
