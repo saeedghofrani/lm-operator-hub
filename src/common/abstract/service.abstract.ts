d773b02c78bf913ca050956a02d216e103a75eac
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-31 01:50:45 +0330

    service => write comments

--- src/common/abstract/service.abstract.ts
+++ src/common/abstract/service.abstract.ts
@@ -5,23 +5,48 @@ import { BaseRepository } from './repository.abstract';
 export abstract class BaseService<T, C, A> {
   constructor(private repository: BaseRepository<T, C, A>) {}
 
-  async create(createDto: C) {
+  /**
+   * Create a new record using the provided DTO.
+   * @param createDto - Data to create the record.
+   * @returns A promise that resolves to the created record.
+   */
+  async create(createDto: C): Promise<T> {
     return this.repository.create(createDto);
   }
 
-  async findAll() {
+  /**
+   * Retrieve all records.
+   * @returns A promise that resolves to an array of records.
+   */
+  async findAll(): Promise<T[]> {
     return this.repository.findAll();
   }
 
-  async findOne(id: number) {
+  /**
+   * Retrieve a single record by its ID.
+   * @param id - The ID of the record to retrieve.
+   * @returns A promise that resolves to the retrieved record or null if not found.
+   */
+  async findOne(id: number): Promise<T | null> {
     return this.repository.findOne(id);
   }
 
-  async update(id: number, updateDto: A) {
+  /**
+   * Update a record by its ID with the provided DTO.
+   * @param id - The ID of the record to update.
+   * @param updateDto - Data to update the record.
+   * @returns A promise that resolves to the updated record or null if not found.
+   */
+  async update(id: number, updateDto: A): Promise<T | null> {
     return this.repository.update(id, updateDto);
   }
 
-  async remove(id: number) {
+  /**
+   * Remove a record by its ID.
+   * @param id - The ID of the record to remove.
+   * @returns A promise that resolves to the removed record or null if not found.
+   */
+  async remove(id: number): Promise<T | null> {
     return this.repository.remove(id);
   }
 }
