e7acb4a7024b68a9962175f5d11457707083101a
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-30 22:36:29 +0330

    guard => set payload to request user

--- src/common/guards/auth.guard.ts
+++ src/common/guards/auth.guard.ts
@@ -7,23 +7,35 @@ import {
 import { Reflector } from '@nestjs/core';
 import { Request } from 'express';
 import { IS_PUBLIC_KEY } from '../constant/public.constant';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(
+    private reflector: Reflector,
+    private jwtService: JwtService
+    ) { }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-    if (isPublic) {
-      return true;
-    }
-    const request = context.switchToHttp().getRequest();
-    const token = this.extractTokenFromHeader(request);
-    if (!token) {
-      throw new UnauthorizedException();
+    try {
+      const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+        context.getHandler(),
+        context.getClass(),
+      ]);
+      if (isPublic) {
+        return true;
+      }
+      const request = context.switchToHttp().getRequest();
+      const token = this.extractTokenFromHeader(request);
+      if (!token)
+        throw new UnauthorizedException();
+
+      const payload = await this.jwtService.verifyAsync(
+        token,
+      );
+      request.user = payload;
+    } catch {
+      throw new UnauthorizedException('you are not authorized');
     }
     return true;
   }
