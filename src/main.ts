c0391c7b7da2af25d1933a0f49df90f78d662917
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-30 11:37:33 +0330

    swagger => bind swagger to env

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
-import { AppModule } from './api/app/app.module';
 import { Logger } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
+import { AppModule } from './api/app/app.module';
 import { HttpExceptionFilter } from './common/exception/error.exeption';
 import { AppConfigService } from './config/app/app-config.service';
-import helmet from 'helmet';
+import { SwaggerConfigService } from './config/swagger/swagger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfigService = <AppConfigService>app.get(AppConfigService);
+  const swaggerConfig = app.get<SwaggerConfigService>(SwaggerConfigService);
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.setGlobalPrefix(appConfigService.appApiPrefix);
   app.useGlobalFilters(new HttpExceptionFilter(httpAdapter));
   app.use(helmet({crossOriginResourcePolicy: false,}));
   app.enableCors({origin: '*'});
+  swaggerConfig.init(app);
   await app.listen(appConfigService.appPort).then(async () => {
     Logger.log(`Swagger Is Enable In development Mode`, 'Swagger');
     Logger.log(`Running`, 'Swagger');
