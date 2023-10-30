4b94638ee64f5462fe0bd151d2ac41dbd9e452a2
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-30 09:32:07 +0330

    error => add exeption handler

--- src/common/exception/error.exeption.ts
+++ src/common/exception/error.exeption.ts
@@ -1,53 +1,63 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
-import { Request, Response } from 'express';
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import { PrismaClientKnownRequestError, PrismaClientRustPanicError, PrismaClientValidationError, PrismaClientUnknownRequestError, PrismaClientInitializationError } from '@prisma/client/runtime/library';
+import { Response } from 'express';
 
-@Catch(HttpException, PrismaClientKnownRequestError) // Catch Prisma-specific error
-export class HttpExceptionFilter implements ExceptionFilter {
-    constructor() { }
+@Catch(HttpException, PrismaClientKnownRequestError, PrismaClientRustPanicError, PrismaClientValidationError, PrismaClientUnknownRequestError, PrismaClientInitializationError)
+export class HttpExceptionFilter extends BaseExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException | PrismaClientKnownRequestError | PrismaClientRustPanicError | PrismaClientValidationError | PrismaClientUnknownRequestError | PrismaClientInitializationError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status = this.getHttpStatus(exception);
+    const message = this.getErrorMessage(exception);
+    const date = Date.now();
+    this.sendResponse(response, status, message, date);
+  }
 
-    async catch(exception: HttpException | PrismaClientKnownRequestError, host: ArgumentsHost) {
-        const ctx = host.switchToHttp();
-        const response = ctx.getResponse<Response>();
-
-        // Determine the HTTP status code
-        const status =
-            exception instanceof HttpException ? exception.getStatus() : 500;
-
-        const date = Date.now();
-
-        // Define error messages and responses based on status code
-        const errorResponse: any = {
-            400: {
-                msg: 'Bad Request',
-                status: 'failed',
-            },
-            401: { msg: 'Unauthorized', status: 'failed' },
-            403: {
-                msg: 'Forbidden',
-                status: 'failed',
-            },
-            404: {
-                msg: 'Not Found',
-                status: 'failed',
-            },
-            500: { msg: 'Internal Server Error', status: 'failed' },
-        };
-
-        let errorMessage = errorResponse[status] || errorResponse[500]; // Default to Internal Server Error
-
-        // Handle Prisma-specific errors
-        if (exception instanceof PrismaClientKnownRequestError) {
-            errorMessage = {
-                msg: exception.message,
-                status: 'failed',
-            };
-        }
-
-        // Send the appropriate response based on the status code
-        response.status(status).json({
-            ...errorMessage,
-            timestamp: date,
-        });
+  private getHttpStatus(exception: HttpException | PrismaClientKnownRequestError | PrismaClientRustPanicError | PrismaClientValidationError | PrismaClientUnknownRequestError | PrismaClientInitializationError): HttpStatus {
+    if (exception instanceof HttpException) {
+      return exception.getStatus();
     }
+    if (exception instanceof PrismaClientKnownRequestError && exception.code === 'P2002') {
+      return HttpStatus.CONFLICT;
+    }
+    if (exception instanceof PrismaClientRustPanicError || exception instanceof PrismaClientValidationError || exception instanceof PrismaClientKnownRequestError || exception instanceof PrismaClientUnknownRequestError || exception instanceof PrismaClientInitializationError) {
+      return HttpStatus.BAD_REQUEST;
+    }
+    return HttpStatus.INTERNAL_SERVER_ERROR;
+  }
+
+  private getErrorMessage(exception: HttpException | PrismaClientKnownRequestError | PrismaClientRustPanicError | PrismaClientValidationError | PrismaClientUnknownRequestError | PrismaClientInitializationError): any {
+    if (exception instanceof PrismaClientKnownRequestError || PrismaClientKnownRequestError || PrismaClientRustPanicError || PrismaClientValidationError || PrismaClientUnknownRequestError || PrismaClientInitializationError) {
+      return {
+        msg: exception.message.replace(/\n/g, ''),
+        status: 'failed',
+      };
+    }
+    return this.getErrorDetails(<HttpException>(exception));
+  }
+
+  private getErrorDetails(exception: HttpException): any {
+    const httpStatus = exception.getStatus();
+    let errorMessage: any;
+
+    switch (httpStatus) {
+      case HttpStatus.BAD_REQUEST:
+        errorMessage = exception.getResponse();
+        break;
+      case HttpStatus.UNPROCESSABLE_ENTITY:
+        errorMessage = exception.getResponse();
+        break;
+      default:
+        errorMessage = 'Sorry! Something went wrong on our end. Please try again later.';
+    }
+
+    return {
+      errors: typeof errorMessage === 'string' ? [errorMessage] : errorMessage,
+    };
+  }
+
+  private sendResponse(response: Response, status: HttpStatus, message: any, date: number) {
+    response.status(status).json({ ...message, timestamp: date });
+  }
 }
