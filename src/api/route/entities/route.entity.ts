230f2914f8caaf74fc7c49c91d2f7856cbe2f0ca
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-30 16:08:21 +0330

    route => define route entity

---
+++ src/api/route/entities/route.entity.ts
@@ -0,0 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { $Enums, Route } from 'prisma/prisma-client';
+import { BaseEntity } from 'src/common/entities/base.entity';
+
+export class RouteEntity extends BaseEntity implements Route {
+  @ApiProperty() deleted: Date;
+  @ApiProperty() address: string;
+  @ApiProperty() method: $Enums.RequestMethod;
+  @ApiProperty() description: string;
+  @ApiProperty() id: number;
+}
