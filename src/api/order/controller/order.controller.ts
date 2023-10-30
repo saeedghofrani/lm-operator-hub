da5afda673eb1d684abfc883c11035e2819ba77d
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-30 09:36:03 +0330

    order => add order controller

---
+++ src/api/order/controller/order.controller.ts
@@ -0,0 +1,48 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { PaginationQueryDto } from 'src/common/pagination/dto/query.dto';
+import { CreateOrderDto } from '../dto/create-order.dto';
+import { UpdateOrderDto } from '../dto/update-order.dto';
+import { OrderEntity } from '../entities/order.entity';
+import { OrderService } from '../service/order.service';
+
+@Controller('order')
+@ApiTags('orders')
+export class OrderController {
+  constructor(private readonly orderService: OrderService) { }
+
+  @Post()
+  @ApiCreatedResponse({ type: OrderEntity })
+  create(@Body() createOrderDto: CreateOrderDto) {
+    return this.orderService.create(createOrderDto);
+  }
+
+  @Get()
+  @ApiOkResponse({ type: OrderEntity, isArray: true })
+  findAll() {
+    return this.orderService.findAll();
+  }
+
+  @Get('page')
+  pagination(@Query() paginationQueryDto: PaginationQueryDto) {
+    return this.orderService.pagination(paginationQueryDto);
+  }
+
+  @Get(':id')
+  @ApiOkResponse({ type: OrderEntity })
+  findOne(@Param('id') id: string) {
+    return this.orderService.findOne(+id);
+  }
+
+  @Patch(':id')
+  @ApiOkResponse({ type: OrderEntity })
+  update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
+    return this.orderService.update(+id, updateOrderDto);
+  }
+
+  @Delete(':id')
+  @ApiOkResponse({ type: OrderEntity })
+  remove(@Param('id') id: string) {
+    return this.orderService.remove(+id);
+  }
+}
