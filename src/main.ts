8ee0f1d2237ad781cd66be5175b4cd198f4cf440
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-31 01:04:37 +0330

    main => seperate concept

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,30 @@
-import { Logger, ValidationPipe } from '@nestjs/common';
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import { AppModule } from './api/app/app.module';
-import { HttpExceptionFilter } from './common/exception/error.exeption';
 import { AppConfigService } from './config/app/app-config.service';
 import { SwaggerConfigService } from './config/swagger/swagger.service';
+import { HttpExceptionFilter } from './common/exception/error.exeption';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const appConfigService = <AppConfigService>app.get(AppConfigService);
-  const swaggerConfig = app.get<SwaggerConfigService>(SwaggerConfigService);
-  const { httpAdapter } = app.get(HttpAdapterHost);
+  try {
+    const app = await NestFactory.create(AppModule);
+    const appConfigService = app.get(AppConfigService);
+    const swaggerConfig = app.get(SwaggerConfigService);
+
+    // Set global configuration
+    configureApp(app, appConfigService, swaggerConfig);
+
+    // Start the application
+    await startApp(app, appConfigService, swaggerConfig);
+  } catch (error) {
+    Logger.error(`Application failed to start: ${error}`, 'Bootstrap');
+  }
+}
+
+function configureApp(app, appConfigService, swaggerConfig) {
   app.setGlobalPrefix(appConfigService.appApiPrefix);
-  app.useGlobalFilters(new HttpExceptionFilter(httpAdapter));
+  app.useGlobalFilters(new HttpExceptionFilter(app.getHttpAdapter()));
   app.use(helmet({ crossOriginResourcePolicy: false }));
   app.enableCors({ origin: '*' });
   app.useGlobalPipes(
@@ -23,17 +35,13 @@ async function bootstrap() {
     }),
   );
   swaggerConfig.init(app);
-  await app.listen(appConfigService.appPort).then(async () => {
-    Logger.log(`Swagger Is Enable In development Mode`, 'Swagger');
-    Logger.log(`Running`, 'Swagger');
-    Logger.log(
-      `http://127.0.0.1:${appConfigService.appPort}/${appConfigService.appApiPrefix}`,
-      'Running Server',
-    );
-    Logger.log(
-      `http://127.0.0.1:${appConfigService.appPort}/${swaggerConfig.preFix}`,
-      'Running Swagger',
-    );
-  });
 }
+
+async function startApp(app: INestApplication<any>, appConfigService: AppConfigService, swaggerConfig: SwaggerConfigService) {
+  await app.listen(appConfigService.appPort);
+  Logger.log('Application is running', 'Bootstrap');
+  Logger.log(`Server: http://127.0.0.1:${appConfigService.appPort}/${appConfigService.appApiPrefix}`, 'Bootstrap');
+  Logger.log(`Swagger: http://127.0.0.1:${appConfigService.appPort}/${swaggerConfig.preFix}`, 'Bootstrap');
+}
+
 bootstrap();
