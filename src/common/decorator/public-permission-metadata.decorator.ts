e4eb1352a7b2ee2421b485c6d6b3255b325eeda6
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-30 22:30:45 +0330

    decorator=> public permission metadata

---
+++ src/common/decorator/public-permission-metadata.decorator.ts
@@ -0,0 +1,4 @@
+import { SetMetadata } from '@nestjs/common';
+import { IS_PUBLIC_PERMISSION_KEY } from '../constant/public-permission.constant';
+
+export const PublicPermission = () => SetMetadata(IS_PUBLIC_PERMISSION_KEY, true);
