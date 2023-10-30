9634b31c4b1ea272de701cb78d71b72942fc51f5
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-30 12:34:37 +0330

    user => define login dto

---
+++ src/api/user/dto/login.dto.ts
@@ -0,0 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+    IsEmail,
+    IsNotEmpty,
+    IsString,
+    Length,
+    Matches
+} from 'class-validator';
+
+export class LoginDto {
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty({ message: 'Mobile number is required' })
+    @IsEmail()
+    email: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty({ message: 'Password is required' })
+    @Length(1, 20, {
+        message: 'Password length must be between 6 and 20 characters',
+    })
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/, {
+        message: 'Password must include at least one uppercase letter, one lowercase letter, one digit, and one special character',
+    })
+    password: string;
+}
