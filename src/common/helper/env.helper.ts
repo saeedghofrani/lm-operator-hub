5519dfa5fc1459967ed9e6bf8d5a6d8c49f48a04
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-31 08:42:47 +0330

    Revert "helper => log envirenment to console"
    This reverts commit ff32f56625b915a225555bd6d66eb9b9dc7903ce.

--- src/common/helper/env.helper.ts
+++ src/common/helper/env.helper.ts
@@ -1,10 +1,10 @@
-import { Logger } from '@nestjs/common';
 import { resolve } from 'path';
 
 export function getEnvPath(): string {
   const prefix = 'src/common/envs';
-  const env: string | 'development' = process.env.APP_MODE;
-  const filename: string = `${prefix}/${env}.env`
-  Logger.warn(`Server is running on ${env}`, 'Running Server')
+  const env: string | undefined = process.env.APP_MODE;
+  const filename: string = env
+    ? `${prefix}/${env}.env`
+    : prefix + '/development.env';
   return resolve(`${filename}`);
 }
