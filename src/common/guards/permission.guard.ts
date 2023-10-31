4bb4dc739687099605cf72759678a54d9ca66bee
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-31 08:28:56 +0330

    guard => cache permission result

--- src/common/guards/permission.guard.ts
+++ src/common/guards/permission.guard.ts
@@ -1,10 +1,11 @@
-import { CanActivate, ExecutionContext, ForbiddenException, Inject, UnauthorizedException, UseGuards } from "@nestjs/common";
-import { PermissionService } from "src/api/permission/service/permission.service";
-import { UserInterface } from "../interfaces/user.interface";
+import { CanActivate, ExecutionContext, ForbiddenException, Inject, UnauthorizedException } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
+import { PermissionService } from "src/api/permission/service/permission.service";
 import { IS_PUBLIC_PERMISSION_KEY } from "../constant/public-permission.constant";
 
 export class PermissionGuard implements CanActivate {
+    private cache: Map<string, number> = new Map(); // Cache to store permission results
+
     constructor(
         @Inject(PermissionService) private permissionService: PermissionService,
         private reflector: Reflector
@@ -24,22 +25,40 @@ export class PermissionGuard implements CanActivate {
         const permissions: number[] = request['user']['permissions'] || [];
         if (permissions.length < 1)
             throw new UnauthorizedException('you are not authorized');
+
+        // Generate a unique cache key based on address and method
+        const cacheKey = `${address}_${method}`;
+
+        if (this.cache.has(cacheKey)) {
+            const permissionId = this.cache.get(cacheKey);
+            return permissions.includes(permissionId);
+        }
+
         address = address.replace(/\/\d+/g, (match) => {
             const param = this.getPropertiesByValue(request.params, match.substring(1));
             return `/{${param[0]}}`
         });
         let permission = await this.permissionService.findByAddress(address, method);
+        
         address = address.replace(/\/\d+/g, (match) => {
             const param = this.getPropertiesByValue(request.params, match.substring(1));
             return `/${param[0]}`
         });
-        if (!permission)
+
+        if (!permission) {
             throw new UnauthorizedException('you are not authorized');
-        const access = permissions.find(item => item == permission.id);
-        if (!access)
+        }
+
+        const access = permissions.includes(permission.id);
+        
+        // Cache the result for future use
+        this.cache.set(cacheKey, permission.id);
+
+        if (!access) {
             throw new ForbiddenException('Forbidden resource');
-        else
+        } else {
             return true;
+        }
     }
 
     getPropertiesByValue(obj, valueToFind) {
