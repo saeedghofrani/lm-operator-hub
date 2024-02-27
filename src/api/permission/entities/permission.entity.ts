17ec44c92b998cd8c7a55286fde924627d40b98a
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2024-02-27 10:24:38 +0330

    test

--- src/api/permission/entities/permission.entity.ts
+++ src/api/permission/entities/permission.entity.ts
@@ -4,6 +4,7 @@ import { BaseEntity } from 'src/common/entities/base.entity'
 
 export class PermissionEntity extends BaseEntity implements Permission {
     @ApiProperty() name: string
+    s?: string
     @ApiProperty() read: $Enums.ReadAccess
     @ApiProperty() deleted: Date
     @ApiProperty() id: number
